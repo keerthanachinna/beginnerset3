03c35a3b8d0d58678f98331646b1b825fd7da27c
keerthanachinna <40225685+keerthanachinna@users.noreply.github.com>
2018-06-24 00:58:47 +0530

    Create swapnum.py

---
+++ swapnum.py
@@ -0,0 +1,15 @@
+def main():
+printf("enter the numbers")
+n1=int(raw_input(enter the n1:))
+n2=int(raw_input(enter the n2:))
+print(n1)
+print(n2)
+  if((n1<100000)&&(n2<100000)):
+  t=n1
+n1=n2
+n2=t
+printf("after swappping the numbers are %d,%d",n1,n2)
+else
+printf("number with in only 100000");
+
+  
