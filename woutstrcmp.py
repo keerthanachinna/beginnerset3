0b2803c6dac1966b3c6a6e9b15a290135aa1b658
keerthanachinna <40225685+keerthanachinna@users.noreply.github.com>
2018-06-24 10:35:08 +0530

    Create woutstrcmp.py

---
+++ woutstrcmp.py
@@ -0,0 +1,21 @@
+def main():
+string1="hello"
+print(len(string1))
+string2="world"
+print(len(string2))
+n1=raw_input(len(sting1))
+n2=raw_input(len(string2))
+	if(n1>n2):
+	{
+		print("\n%s",string1);
+	}
+	elif(n2>n1):
+	{
+		print("\n%s",string2)
+	}
+	else:
+	{
+		printf("\n %s",string2)
+	}
+	return 0
+}
