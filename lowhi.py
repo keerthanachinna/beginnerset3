91c4258d2acdfa861da29bb3b4d8e9685848ef57
keerthanachinna <40225685+keerthanachinna@users.noreply.github.com>
2018-06-24 11:53:01 +0530

    Create lowhi.py

---
+++ lowhi.py
@@ -0,0 +1,13 @@
+n=int(input("enter the number"))
+print(n)
+arr=int(input("enter array element"))
+print(arr)
+for i in range n
+print(arr[i])
+for i in range n
+for j in range i+1
+if(arr[i]>arr[j]):
+t=a[i]
+a[i]=a[j]
+a[j]=t
+printf("largest & smallest: %d %d",a[0],a[n-1])
