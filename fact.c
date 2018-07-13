ba91c4ae4185630f6ebc6e92de1c143acddd4f41
keerthanachinna <40225685+keerthanachinna@users.noreply.github.com>
2018-07-13 10:59:07 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,res,
+printf("enter the number:");
+scanf("%d",&n);
+res=fact(n);
+printf("%d",n)
+fact(int n)
+{
+int result;
+if(n==0)
+res=n*fact(n-1)
+result=1
+return result;
+}
